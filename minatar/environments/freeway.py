e2cf4bbdaabee9d8c24a3983995c2832125558b3
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-12-25 22:14:40 +0900

    change freeway terminal condition

--- minatar/environments/freeway.py
+++ minatar/environments/freeway.py
@@ -70,6 +70,7 @@ class Env:
         for car in self.cars:
             if(car[0:2]==[4,self.pos]):
                 self.pos = 9
+                self.terminal = True
             if(car[2]==0):
                 car[2]=abs(car[3])
                 car[0]+=1 if car[3]>0 else -1
@@ -79,6 +80,7 @@ class Env:
                     car[0]=0
                 if(car[0:2]==[4,self.pos]):
                     self.pos = 9
+                    self.terminal = True
             else:
                 car[2]-=1
 
