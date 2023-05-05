61c5c29e320c57a146cf153a9fe424d89578fddb
Kenny Young <kjyoung@ualberta.ca>
2023-05-05 09:55:01 -0600

    fixed my email domain in setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     description='A miniaturized version of the Arcade Learning Environment.',
     url='https://github.com/kenjyoung/MinAtar',
     author='Kenny Young',
-    author_email='kjyoung@ualberta.com',
+    author_email='kjyoung@ualberta.ca',
     license='GPL',
     packages=packages,
     entry_points=entry_points,
